32688836670991fbf8a9feb829430f85df36a036
MATT-PC\Matt <mobrien55@hotmail.com>
2017-05-19 11:43:01 -0600

    Working on Aim at crosshairs

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,7 +14,10 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
+private:
+	virtual void BeginPlay() override;
+
 	ATank* GetControlledTank() const;
 	
-	virtual void BeginPlay() override;
+	ATank* GetPlayerTank() const;
 };
