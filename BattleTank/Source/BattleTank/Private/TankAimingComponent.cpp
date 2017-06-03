0c0419cc923c6d3f8e862dc9ba6892c22eca0166
MATT-PC\Matt <mobrien55@hotmail.com>
2017-06-03 13:25:47 -0600

    Working particles for shot and explosion

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -49,7 +49,7 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 	}
 }
 
-int UTankAimingComponent::GetRoundsLeft() const
+int32 UTankAimingComponent::GetRoundsLeft() const
 {
 	return RoundsLeft;
 }
